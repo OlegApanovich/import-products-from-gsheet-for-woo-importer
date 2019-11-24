fa37863709ecf1359e9266d0bdb8c9f41326236a
Oleg Apanovich <dollar4444@gmail.com>
2019-11-24 12:42:11 +0200

    Combane new localization file

--- gsheet-for-woo-importer.php
+++ gsheet-for-woo-importer.php
@@ -12,8 +12,8 @@
  * Author URI:   https://github.com/OlegApanovich
  * License:      GPL-3.0+
  * License URI:  http://www.gnu.org/licenses/gpl-3.0.txt
- * Text Domain: woocommerce-import-products-google-sheet
- * Domain Path: /languages
+ * Text Domain:  woocommerce-import-products-google-sheet
+ * Domain Path:  /languages
  */
 defined( 'ABSPATH' ) || exit;
 

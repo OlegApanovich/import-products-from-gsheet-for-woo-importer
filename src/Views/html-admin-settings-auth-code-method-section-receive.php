57429df96dc71f716fac32d0e99ba0e6899ec140
OlegApanovich <dollar4444@gmail.com>
2023-04-30 12:22:45 +0300

    #21 code standard commit

--- src/Views/html-admin-settings-auth-code-method-section-receive.php
+++ src/Views/html-admin-settings-auth-code-method-section-receive.php
@@ -10,6 +10,14 @@
  */
 
 defined( 'ABSPATH' ) || exit;
+
+// @codingStandardsIgnoreLine
+$code = empty( $_GET['code'] ) ? '' : rawurldecode( $_GET['code'] );
+// we need it to know where to redirect after authorization
+$plugin_setting_page = '&page=' . ( admin_url( 'admin.php?page=woocommerce_import_products_google_sheet_menu' ) );
+$app_service_link    = 'https://monolitpro.info?plugin=import-products-from-gsheet-for-woo-importer?';
+$action_name         = '&action=connect-redirect';
+$app_link            = $app_service_link . $plugin_setting_page . $action_name;
 ?>
 
 <h3>
@@ -18,8 +26,15 @@ defined( 'ABSPATH' ) || exit;
 	</label>
 </h3>
 
-<a class="button-primary" href="https://monolitpro.info?plugin=import-products-from-gsheet-for-woo-importer&action=connect-redirect&page=<?php echo urlencode( admin_url( 'admin.php?page=woocommerce_import_products_google_sheet_menu' ) ); ?>">
+<a class="button-primary" href="<?php echo esc_html( $app_link ); ?>">
 	<strong><?php esc_html_e( 'Get Code', 'import-products-from-gsheet-for-woo-importer' ); ?></strong>
 </a>
 
-<input type="password" id="plugin_google_oauth2_code" placeholder="Enter Code" name="plugin_wc_import_google_sheet_options[google_code_oauth2]" size="40" value="<?php echo empty( $_GET['code'] ) ? '' : urldecode( $_GET['code'] ); ?>">
+<input
+	type="password"
+	id="plugin_google_oauth2_code"
+	placeholder="Enter Code"
+	name="plugin_wc_import_google_sheet_options[google_code_oauth2]"
+	size="40"
+	value="<?php echo esc_html( $code ); ?>"
+>
